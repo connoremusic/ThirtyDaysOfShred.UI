4162d30c471b6d5581559d60fab44e0ecaff256d
Connor Engstrom <connor.e.music@gmail.com>
2022-09-24 15:24:06 -0400

    Added basic routing

--- thirtydaysofshred-ui/src/app/app.module.ts
+++ thirtydaysofshred-ui/src/app/app.module.ts
@@ -12,13 +12,23 @@ import { Component } from '@angular/core';
 import { BsDropdownModule, BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +37,7 @@ import { RegisterComponent } from './register/register.component';
     TooltipModule.forRoot(),
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }],
   bootstrap: [AppComponent]
